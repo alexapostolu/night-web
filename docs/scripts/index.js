41b758bd3854531c36ebb827d55441cd4b418f01
alexapostolu <apostolu240@gmail.com>
2025-03-25 17:06:30 -0400

    dynamically fecthed latest version

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,9 +1,15 @@
 let time = 0;
 let stars = [];
-let width, height;
+let canvas_height = 500;
 
-class Star {
-	constructor() {
+let frameCountStart = 0;
+let testDuration = 10000; // 5 seconds in milliseconds
+let testRunning = false;
+
+class Star
+{
+	constructor()
+	{
 		this.x = random(0, width);
 		this.y = random(0, height);
 		this.brightness = random(0, 255);
@@ -12,65 +18,69 @@ class Star {
 			this.inc *= -1;
 	}
 
-	display() {
-		fill(255, 255, 255, this.brightness);
+	display()
+	{
 		noStroke();
+		fill(255, 255, 255, this.brightness);
 	  	circle(this.x, this.y, 1.5);
 
 		this.brightness += this.inc;
 
-		if (this.brightness >= 255) {
+		if (this.brightness >= 255)
+		{
 			this.inc *= -1;
 		}
-		else if (this.brightness <= 0) {
+		else if (this.brightness <= 0)
+		{
 			this.inc *= -1;
 			this.x = random(0, width);
-			this.y = random(0, height);
+			this.y = random(0, canvas_height);
 		}
 	}
 }
 
 function setup() {
-	width = windowWidth;
-	height = 500;
 	createCanvas(windowWidth - 17, 500);
 
 	for (let a = 0; a < 50; ++a)
 		stars.push(new Star());
 
 	requestAnimationFrame(draw);
+
+	frameCountStart = millis();
+	testRunning = true;
 }
 
-function draw() {
-	// Background.
-
-	clear();
-
-	fill(0);
-	rect(0, 0, width, height);
+function draw()
+{
+	background(0);
 
 	noStroke();
 
-	let clr = (Math.sin(time) * 50) + 10;
-	for (let d = (clr + 150); d >= 0; d -= 1) {
-		fill(128 - (d / (clr + 150)) * 128);
-		circle(0, 0, d * (height / 50));
+	let clr = (Math.sin(time) * 50) + 160;
+	let h = canvas_height / 50;
+	for (let d = (clr + 150); d >= 0; d -= 1)
+	{
+		fill(128 - (d / clr) * 128);
+		circle(0, 0, d * h);
 	}
 
 	time += 0.005;
 
-	// Stars and cursor lines.
-
-	for (let a = 0; a < stars.length; ++a) {
-		stars[a].display();
+	for (let i = 0; i < stars.length; ++i)
+	{
+		stars[i].display();
 		
-		if (a % 2 == 0 && dist(stars[a].x, stars[a].y, mouseX, mouseY) < 200) {
-			stroke(255);
-			strokeWeight(2);
-			line(stars[a].x, stars[a].y, mouseX, mouseY);
+		if (i % 2 === 0)
+		{
+			let dx = abs(stars[i].x - mouseX);
+			let dy = abs(stars[i].y - mouseY);
+			if (dx + dy < 200)
+			{
+				stroke(255);
+				strokeWeight(2);
+				line(stars[i].x, stars[i].y, mouseX, mouseY);
+			}
 		}
 	}
-
-	requestAnimationFrame(draw);
-	DelayNode(0.02);
 }
